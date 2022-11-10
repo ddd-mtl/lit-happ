06de0717d4f0a4fe1b855008299823a1a8df27e4
damien <doute.d@gmail.com>
2022-11-10 16:06:24 -0500

    Implemented anyToB64() and  zomeName reporting

--- src/dnaClient.ts
+++ src/dnaClient.ts
@@ -40,6 +40,31 @@ export class DnaClient {
   private _responseLog: ZomeCallResponse[] = []
 
 
+
+  private anyToB64(obj: any): any {
+    /** Check if its a hash */
+    if (obj instanceof Uint8Array) {
+      return serializeHash(obj);
+    } else {
+      /** Check if its an array of hashes */
+      if (Array.isArray(obj)) {
+        const isUint8Array =
+          obj.length > 0 &&
+          obj.every((value) => {
+            return value instanceof Uint8Array;
+          });
+        if (isUint8Array) {
+          let result = [];
+          for (const cur of obj) {
+            result.push(serializeHash(cur));
+          }
+          return result;
+        }
+      }
+    }
+    return obj;
+  }
+
   /** -- Methods -- */
 
   /** */
@@ -47,13 +72,14 @@ export class DnaClient {
     //console.log("callZome: agent_directory." + fn_name + "() ", payload)
     //console.info({payload})
     const requestIndex = this._requestLog.length;
-    const request: ZomeCallRequest = {zomeName, fnName, payload, timestamp: Date.now()}
+    const request: ZomeCallRequest = {zomeName, fnName, timestamp: Date.now(), payload: this.anyToB64(payload)};
     this._requestLog.push(request)
     try {
       const result = await this.agnosticClient.callZome(this.cellId, zomeName, fnName, payload, this.defaultTimeout);
       //console.log("callZome: agent_directory." + fn_name + "() result")
       //console.info({result})
-      this._responseLog.push({success: result, timestamp: Date.now(), requestIndex})
+      let success = this.anyToB64(result)
+      this._responseLog.push({success, timestamp: Date.now(), requestIndex})
       return result;
     } catch (e) {
       console.error("Calling zome + " + zomeName + "." + fnName + "() failed: ")
@@ -73,12 +99,14 @@ export class DnaClient {
         continue;
       }
       const startDate = new Date(request.timestamp);
-      const startTime = "" + startDate.getHours() + ":" + startDate.getMinutes() + "." + startDate.getMilliseconds()
-      const log = {
-        startTime, zomeName: request.zomeName, fnName: request.fnName, input: request.payload, output: response.success? response.success: response.failure
-      }
+      const startTime = "" + startDate.getHours() + ":" + startDate.getMinutes() + "." + startDate.getMilliseconds();
+      const input = request.payload instanceof Uint8Array? serializeHash(request.payload) : request.payload;
+      const output = response.failure? response.failure: response.success;
+      const log = zomeName? {startTime,fnName: request.fnName, input, output}
+        : {startTime, zomeName: request.zomeName, fnName: request.fnName, input, output}
       result.push(log)
     }
+    if(zomeName) console.log("Call log for zome " + zomeName)
     console.table(result)
   }
 
