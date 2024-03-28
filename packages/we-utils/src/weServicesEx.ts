08a01c843b62821df637095cac57c2194bed5bfa
damien <doute.d@gmail.com>
2024-03-28 10:18:56 -0400

    better API for weServicesEx

--- packages/we-utils/src/weServicesEx.ts
+++ packages/we-utils/src/weServicesEx.ts
@@ -5,22 +5,40 @@ import {
   WeServices,
 } from "@lightningrodlabs/we-applet";
 import {
-  AppletHash, AssetLocationAndInfo, FrameNotification, OpenWalMode, WAL,
+  AssetLocationAndInfo, FrameNotification, GroupProfile, OpenWalMode, WAL,
 } from "@lightningrodlabs/we-applet/dist/types";
-import {ActionHash, DnaHash, DnaHashB64, encodeHashToBase64, EntryHash, EntryHashB64} from "@holochain/client";
+import {
+  ActionHash,
+  decodeHashFromBase64,
+  DnaHash,
+  DnaHashB64,
+  encodeHashToBase64,
+  EntryHash,
+  EntryHashB64
+} from "@holochain/client";
+
+
+/** */
+export interface WeServicesCache {
+  assetInfo: Record<string, AssetLocationAndInfo | undefined>;
+  groupProfile: Record<string, any | undefined>;
+  appletInfo: Record<AppletId, AppletInfo | undefined>;
+}
 
 
 /** WeServices wrapper that caches requested infos */
 export class WeServicesEx implements WeServices {
 
-  //attachmentTypes: ReadonlyMap<AppletHash, Record<AttachmentName, AttachmentType>>;
-
   constructor(private _inner: WeServices, private _thisAppletId: AppletId) {
-    //this.attachmentTypes = _inner.attachmentTypes;
+    this.cacheFullAppletInfo(_thisAppletId).then(([_appletId, groupProfiles]) => {
+      this._groupProfiles = groupProfiles;
+    })
   }
 
+  /** groupId -> groupProfile */
+  private _groupProfiles: Record<DnaHashB64, GroupProfile> = {};
   /** wurl -> AssetLocationAndInfo */
-  private _attachableInfoCache: Record<string, AssetLocationAndInfo | undefined> = {};
+  private _assetInfoCache: Record<string, AssetLocationAndInfo | undefined> = {};
   /** DnaHashB64 -> groupProfile */
   private _groupProfileCache: Record<string, any | undefined> = {};
   /** appletId -> AppletInfo */
@@ -29,52 +47,71 @@ export class WeServicesEx implements WeServices {
 
   /** -- Getters -- */
 
+  get cache(): WeServicesCache {
+    return {
+      assetInfo: this._assetInfoCache,
+      groupProfile: this._groupProfileCache,
+      appletInfo: this._appletInfoCache,
+    };
+  }
+
   get appletId(): AppletId {return this._thisAppletId}
 
-  getAssetInfo(wal_or_wurl: WAL | string): AssetLocationAndInfo | undefined {
+  get groupProfiles(): Record<DnaHashB64, GroupProfile> {return this._groupProfiles}
+
+  assetInfoCached(wal_or_wurl: WAL | string): AssetLocationAndInfo | undefined {
     let wurl = wal_or_wurl as string;
     if (typeof wal_or_wurl == 'object') {
       wurl = weaveUrlFromWal({hrl: wal_or_wurl.hrl});
     }
-    return this._attachableInfoCache[wurl];
+    return this._assetInfoCache[wurl];
   }
-  getAttachables(): string[] {
-    return Object.keys(this._attachableInfoCache);
-  }
-
-  getGroupProfile(groupdId: DnaHash | DnaHashB64): any | undefined {
+  groupProfileCached(groupdId: DnaHash | DnaHashB64): any | undefined {
     let index = groupdId as DnaHashB64;
     if (typeof groupdId != 'string') {
       index = encodeHashToBase64(groupdId);
     }
     return this._groupProfileCache[index];
   }
-  getGroupIds(): DnaHashB64[] {
-    return Object.keys(this._groupProfileCache);
-  }
-
-  getAppletInfo(appletId: EntryHash | EntryHashB64): AppletInfo | undefined {
+  appletInfoCached(appletId: EntryHash | EntryHashB64): AppletInfo | undefined {
     let index = appletId as EntryHashB64;
     if (typeof appletId != 'string') {
       index = encodeHashToBase64(appletId);
     }
     return this._appletInfoCache[index];
   }
-  getAppletIds(): EntryHashB64[] {
-    return Object.keys(this._appletInfoCache);
-  }
 
 
   /** -- Call & cache info  -- */
 
   /** */
+  async cacheFullAppletInfo(appletId: EntryHash | EntryHashB64): Promise<[AppletInfo, Record<DnaHashB64, GroupProfile>] | undefined> {
+    let index = appletId as EntryHash;
+    if (typeof appletId == 'string') {
+      index = decodeHashFromBase64(appletId);
+    }
+    /* Grab appletInfo and all groupProfiles */
+    const appletInfo = await this.appletInfo(index);
+    if (!appletInfo) {
+      return undefined;
+    }
+    const groupProfiles: Record<DnaHashB64, GroupProfile> = {};
+    for (const groupHash of appletInfo.groupsIds) {
+      const gp = await this.groupProfile(groupHash);
+      groupProfiles[encodeHashToBase64(groupHash)] = gp;
+    }
+    return [appletInfo, groupProfiles];
+  }
+
+
+  /** */
   async assetInfo(wal: WAL): Promise<AssetLocationAndInfo | undefined> {
     const wurl = weaveUrlFromWal({hrl: wal.hrl});
-    if (this._attachableInfoCache[wurl]) {
-      return this._attachableInfoCache[wurl];
+    if (this._assetInfoCache[wurl]) {
+      return this._assetInfoCache[wurl];
     }
-    this._attachableInfoCache[wurl] = await this._inner.assetInfo(wal);
-    return this._attachableInfoCache[wurl];
+    this._assetInfoCache[wurl] = await this._inner.assetInfo(wal);
+    return this._assetInfoCache[wurl];
   }
 
 
