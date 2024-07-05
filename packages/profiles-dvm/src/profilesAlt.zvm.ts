d5e4720affcabc1ace7d0ddafdc2d9883c2d09ee
damien <doute.d@gmail.com>
2024-07-05 13:59:37 -0400

    Implemented dumpSignalLogs for ProfilesAltZvm

--- packages/profiles-dvm/src/profilesAlt.zvm.ts
+++ packages/profiles-dvm/src/profilesAlt.zvm.ts
@@ -1,8 +1,20 @@
 import {
-  ActionId, ActionIdMap,
+  ActionId,
+  ActionIdMap,
   AgentId,
-  AgentIdMap, EntryId,
-  LinkPulseMat,
+  AgentIdMap,
+  enc64,
+  EntryId,
+  getVariantByIndex,
+  LinkPulseMat, materializeEntryPulse, materializeLinkPulse,
+  prettyDate,
+  prettyState,
+  SignalLog,
+  SignalType,
+  TipProtocol,
+  ZomeSignal,
+  ZomeSignalProtocol,
+  ZomeSignalProtocolType,
   ZomeViewModelWithSignals
 } from "@ddd-qc/lit-happ";
 import {Profile} from "./bindings/profiles.types";
@@ -15,6 +27,7 @@ import {
 } from "./bindings/profilesAlt.types";
 import {EntryPulseMat} from "@ddd-qc/lit-happ/dist/ZomeViewModelWithSignals";
 import {ProfilesLinkType} from "./bindings/profiles.integrity";
+import {ProfilesAltLinkType, ProfilesAltUnitEnum} from "./bindings/profilesAlt.integrity";
 
 
 /** */
@@ -162,6 +175,38 @@ export class ProfilesAltZvm extends ZomeViewModelWithSignals {
     }
   }
 
+
+  /** */
+  dumpSignalLogs(signalLogs: SignalLog[]) {
+    this.dumpCastLogs();
+    console.warn(`Signals received from zome "${this.zomeName}"`);
+    let appSignals: any[] = [];
+    signalLogs
+      .filter((log) => log.type == SignalType.Zome)
+      .map((log) => {
+        const signal = log.zomeSignal as ZomeSignal;
+        const pulses = signal.pulses as ZomeSignalProtocol[];
+        const timestamp = prettyDate(new Date(log.ts));
+        const from = enc64(signal.from) == this.cell.agentId.b64? "self" : new AgentId(signal.from);
+        for (const pulse of pulses) {
+          if (ZomeSignalProtocolType.Tip in pulse) {
+            const tip: TipProtocol = pulse.Tip;
+            const type = Object.keys(tip)[0];
+            appSignals.push({timestamp, from, pulse: ZomeSignalProtocolType.Tip, type, payload: tip});
+          }
+          if (ZomeSignalProtocolType.Entry in pulse) {
+            const entryPulse = materializeEntryPulse(pulse.Entry, Object.values(ProfilesAltUnitEnum));
+            const typedEntry = decode(entryPulse.bytes);
+            appSignals.push({timestamp, from, pulse: ZomeSignalProtocolType.Entry, state: prettyState(pulse.Entry.state), type: entryPulse.entryType, payload: typedEntry, ah_base: entryPulse.ah.short, eh_target: entryPulse.eh.short});
+          }
+          if (ZomeSignalProtocolType.Link in pulse) {
+              const linkPulse = materializeLinkPulse(pulse.Link, Object.values(ProfilesAltLinkType));
+              appSignals.push({timestamp, from, pulse: ZomeSignalProtocolType.Link, state: prettyState(pulse.Link.state), type: linkPulse.link_type, payload: linkPulse.tag, ah_base: linkPulse.base.print(), eh_target: linkPulse.target.print()});
+          }
+        }
+      });
+    console.table(appSignals);
+  }
 
 
   /** -- Store -- */
