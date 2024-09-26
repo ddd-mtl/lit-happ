a8d51784622d6d648eba5dbea9f7dcb06e820c73
damien <doute.d@gmail.com>
2024-09-26 15:01:54 -0400

    network stuff for HappMulti

--- packages/lit-happ/src/HappMultiElement.ts
+++ packages/lit-happ/src/HappMultiElement.ts
@@ -1,22 +1,18 @@
 import {LitElement} from "lit";
 import { state } from "lit/decorators.js";
 import {
-  //BaseRoleName,
   AppProxy,
   ConductorAppProxy,
-  //flattenCells,
-  //CellIdStr, AgentIdMap, DnaId, CellAddress,
+  flattenCells,
+  CellIdStr, AgentIdMap, DnaId, CellAddress,
 } from "@ddd-qc/cell-proxy";
 import {HappViewModel} from "./HappViewModel";
 import {/*CellDef,*/ HvmDef} from "./definitions";
 import {
   AppWebsocket,
-  //ClonedCell,
   InstalledAppId,
-  //NetworkInfo, Timestamp
+  NetworkInfo, Timestamp
 } from "@holochain/client";
-//import {DnaViewModel} from "./DnaViewModel";
-//import {CellId} from "@holochain/client/lib/types";
 // @ts-ignore
 import * as net from "net";
 
@@ -35,9 +31,9 @@ export class HappMultiElement extends LitElement {
   get count(): number {return this.hvms.length}
 
   /** Ctor */
-  protected constructor(wtf: [number | AppWebsocket, InstalledAppId | undefined][], adminUrl?: URL, defaultTimeout?: number) {
+  protected constructor(appInfo: [number | AppWebsocket, InstalledAppId | undefined][], adminUrl?: URL, defaultTimeout?: number) {
     super();
-    /* await */ this.constructHvms(wtf, adminUrl, defaultTimeout);
+    /* await */ this.constructHvms(appInfo, adminUrl, defaultTimeout);
   }
 
   /** */
@@ -53,21 +49,16 @@ export class HappMultiElement extends LitElement {
   }
 
   /** */
-  protected async constructHvms(wtf: [number | AppWebsocket, InstalledAppId | undefined][], adminUrl?: URL, defaultTimeout?: number): Promise<void> {
+  protected async constructHvms(appInfo: [number | AppWebsocket, InstalledAppId | undefined][], adminUrl?: URL, defaultTimeout?: number): Promise<void> {
     const hvmDef = (this.constructor as typeof HappMultiElement).HVM_DEF;
     if (!hvmDef) {
       throw Error("HVM_DEF static field undefined in HappMultiElement subclass " + this.constructor.name);
     }
-    for (const [port_or_socket, appId] of wtf) {
-      /** Override appId */
-      if (appId) {
-        hvmDef.id = appId;
-      }
+    for (const [port_or_socket, appId] of appInfo) {
       const appProxy = await ConductorAppProxy.new(port_or_socket, hvmDef.id, adminUrl, defaultTimeout);
-      // @ts-ignore
-      if(!this.appProxy) {
-        // @ts-ignore
-        this.appProxy = appProxy;
+      if (appId) {
+        /** Override appId */
+        hvmDef.id = appId;
       }
       const hvm = await HappViewModel.new(this, appProxy, hvmDef);
       await hvm.authorizeAllZomeCalls(appProxy.adminWs);
@@ -95,132 +86,135 @@ export class HappMultiElement extends LitElement {
   }
 
 
-  //
-  // /** */
-  // async networkInfoAll(baseRoleName?: string): Promise<Record<CellIdStr, [Timestamp, NetworkInfo]>> {
-  //   console.log(`networkInfoAll() "${baseRoleName}"`);
-  //   /** Grab cellMap */
-  //   const hvmDef = (this.constructor as typeof HappElement).HVM_DEF;
-  //   const cellMap = this.appProxy.getAppCells(hvmDef.id);
-  //   if (!cellMap) {
-  //     return Promise.reject("No cells found at given appId: " + hvmDef.id);
-  //   }
-  //   /** Get cell Ids */
-  //   let cellAddrs: CellAddress[] = [];
-  //   if (baseRoleName) {
-  //     const cfr = cellMap[baseRoleName];
-  //     if (!cfr) {
-  //       return Promise.reject("No cells found at given baseRoleName: " + baseRoleName);
-  //     }
-  //     cellAddrs = flattenCells(cfr);
-  //   } else {
-  //     for (const cells of Object.values(cellMap)) {
-  //       cellAddrs = cellAddrs.concat(flattenCells(cells))
-  //     }
-  //   }
-  //   console.log(`networkInfoAll() cellIds`, cellAddrs.map(cellId => cellId.str));
-  //   /* Sort by agent key */
-  //   let dnaPerAgentMap: AgentIdMap<DnaId[]> = new AgentIdMap();
-  //   for (const cellAddr of cellAddrs) {
-  //     if (!dnaPerAgentMap.get(cellAddr.agentId)) {
-  //       dnaPerAgentMap.set(cellAddr.agentId, []);
-  //     }
-  //     dnaPerAgentMap.get(cellAddr.agentId)!.push(cellAddr.dnaId);
-  //   }
-  //   console.log(`networkInfoAll() dnaMap`, dnaPerAgentMap);
-  //   /** Call NetworkInfo per AgentId */
-  //   const allNetInfos: Record<CellIdStr, [Timestamp, NetworkInfo]> = {};
-  //   for (const [agent, dnaIds] of dnaPerAgentMap.entries()) {
-  //     const netInfos = await this.appProxy.networkInfo({dnas: dnaIds.map((dna) => dna.hash)});
-  //     let i  = 0;
-  //     for (const netInfo of netInfos) {
-  //       const idStr = new CellAddress(dnaIds[i]!, agent).str;
-  //       allNetInfos[idStr] = [Date.now(), netInfo];
-  //       i += 1;
-  //     }
-  //   }
-  //   /* Done */
-  //   return allNetInfos;
-  // }
 
-  //
-  // /** */
-  // dumpLastestNetworkInfo(baseRoleName?: string) {
-  //   console.log(`dumpLastestNetworkInfo() "${baseRoleName}"`);
-  //   /** Grab cellMap */
-  //   const hvmDef = (this.constructor as typeof HappElement).HVM_DEF;
-  //   const cellMap = this.appProxy.getAppCells(hvmDef.id);
-  //   if (!cellMap) {
-  //     throw Error("No cells found at given appId: " + hvmDef.id);
-  //   }
-  //   /** Get cell Ids */
-  //   let cellAddrs: CellAddress[] = [];
-  //   if (baseRoleName) {
-  //     const cfr = cellMap[baseRoleName];
-  //     if (!cfr) {
-  //       throw Promise.reject("No cells found at given baseRoleName: " + baseRoleName);
-  //     }
-  //     cellAddrs = flattenCells(cfr);
-  //   } else {
-  //     for (const cells of Object.values(cellMap)) {
-  //       cellAddrs = cellAddrs.concat(flattenCells(cells))
-  //     }
-  //   }
-  //   let logs = cellAddrs.map((cellAddr) => {
-  //     const logs = this.appProxy.networkInfoLogs[cellAddr.str];
-  //     if (!logs || logs.length === 0) {
-  //       return;
-  //     }
-  //     const [ts, info] = logs[logs.length - 1]!;
-  //     const hcl = this.appProxy.getLocations(cellAddr)!;
-  //     const cellName = this.appProxy.getCellName(hcl[0]!);
-  //     return {
-  //           ts,
-  //           name: cellName,
-  //           dna: cellAddr.dnaId.short,
-  //           arc: info.arc_size,
-  //           peers: info.current_number_of_peers,
-  //           total_peers: info.total_network_peers,
-  //           rounds: info.completed_rounds_since_last_time_queried,
-  //           bytes: info.bytes_since_last_time_queried,
-  //           fetch_bytes: info.fetch_pool_info.op_bytes_to_fetch,
-  //           fetch_ops: info.fetch_pool_info.num_ops_to_fetch,
-  //           agent: cellAddr.agentId.short,
-  //         }
-  //   })
-  //   console.table(logs);
-  // }
-  //
-  //
-  // /* */
-  // dumpNetworkInfoLogs(cellIdStr?: string) {
-  //   console.log(`dumpNetworkInfoLogs() "${cellIdStr}"`);
-  //   let logMap = this.appProxy.networkInfoLogs;
-  //   if (cellIdStr) {
-  //     logMap = {};
-  //     logMap[cellIdStr] = this.appProxy.networkInfoLogs[cellIdStr]!;
-  //   }
-  //   for (const [cellIdStr, infoPairs] of Object.entries(logMap)) {
-  //     const cellAddr = CellAddress.from(cellIdStr);
-  //     const hcl = this.appProxy.getLocations(cellAddr)!;
-  //     const cellName = this.appProxy.getCellName(hcl[0]!);
-  //     console.log(`NetworfInfo logs of cell "${cellName}" | [${cellAddr.agentId.short}, ${cellAddr.dnaId.short}]`);
-  //     const logs = infoPairs
-  //       .map(([ts, info]) => {
-  //         return {
-  //           ts,
-  //           arc: info.arc_size,
-  //           peers: info.current_number_of_peers,
-  //           total_peers: info.total_network_peers,
-  //           rounds: info.completed_rounds_since_last_time_queried,
-  //           bytes: info.bytes_since_last_time_queried,
-  //           fetch_bytes: info.fetch_pool_info.op_bytes_to_fetch,
-  //           fetch_ops: info.fetch_pool_info.num_ops_to_fetch,
-  //         }
-  //       });
-  //     console.table(logs);
-  //   }
-  // }
+  /** */
+  async networkInfoAll(baseRoleName?: string): Promise<Record<CellIdStr, [Timestamp, NetworkInfo]>> {
+    console.log(`networkInfoAll() "${baseRoleName}"`);
+    /** Grab cellMap */
+    const hvmDef = (this.constructor as typeof HappMultiElement).HVM_DEF;
+    const appProxy = this.hvms[0]![0];
+    const cellMap = appProxy.getAppCells(hvmDef.id);
+    if (!cellMap) {
+      return Promise.reject("No cells found at given appId: " + hvmDef.id);
+    }
+    /** Get cell Ids */
+    let cellAddrs: CellAddress[] = [];
+    if (baseRoleName) {
+      const cfr = cellMap[baseRoleName];
+      if (!cfr) {
+        return Promise.reject("No cells found at given baseRoleName: " + baseRoleName);
+      }
+      cellAddrs = flattenCells(cfr);
+    } else {
+      for (const cells of Object.values(cellMap)) {
+        cellAddrs = cellAddrs.concat(flattenCells(cells))
+      }
+    }
+    console.log(`networkInfoAll() cellIds`, cellAddrs.map(cellId => cellId.str));
+    /* Sort by agent key */
+    let dnaPerAgentMap: AgentIdMap<DnaId[]> = new AgentIdMap();
+    for (const cellAddr of cellAddrs) {
+      if (!dnaPerAgentMap.get(cellAddr.agentId)) {
+        dnaPerAgentMap.set(cellAddr.agentId, []);
+      }
+      dnaPerAgentMap.get(cellAddr.agentId)!.push(cellAddr.dnaId);
+    }
+    console.log(`networkInfoAll() dnaMap`, dnaPerAgentMap);
+    /** Call NetworkInfo per AgentId */
+    const allNetInfos: Record<CellIdStr, [Timestamp, NetworkInfo]> = {};
+    for (const [agent, dnaIds] of dnaPerAgentMap.entries()) {
+      const netInfos = await appProxy.networkInfo({dnas: dnaIds.map((dna) => dna.hash)});
+      let i  = 0;
+      for (const netInfo of netInfos) {
+        const idStr = new CellAddress(dnaIds[i]!, agent).str;
+        allNetInfos[idStr] = [Date.now(), netInfo];
+        i += 1;
+      }
+    }
+    /* Done */
+    return allNetInfos;
+  }
+
+
+  /** */
+  dumpLastestNetworkInfo(baseRoleName?: string) {
+    console.log(`dumpLastestNetworkInfo() "${baseRoleName}"`);
+    const appProxy = this.hvms[0]![0];
+    /** Grab cellMap */
+    const hvmDef = (this.constructor as typeof HappMultiElement).HVM_DEF;
+    const cellMap = appProxy.getAppCells(hvmDef.id);
+    if (!cellMap) {
+      throw Error("No cells found at given appId: " + hvmDef.id);
+    }
+    /** Get cell Ids */
+    let cellAddrs: CellAddress[] = [];
+    if (baseRoleName) {
+      const cfr = cellMap[baseRoleName];
+      if (!cfr) {
+        throw Promise.reject("No cells found at given baseRoleName: " + baseRoleName);
+      }
+      cellAddrs = flattenCells(cfr);
+    } else {
+      for (const cells of Object.values(cellMap)) {
+        cellAddrs = cellAddrs.concat(flattenCells(cells))
+      }
+    }
+    let logs = cellAddrs.map((cellAddr) => {
+      const logs = appProxy.networkInfoLogs[cellAddr.str];
+      if (!logs || logs.length === 0) {
+        return;
+      }
+      const [ts, info] = logs[logs.length - 1]!;
+      const hcl = appProxy.getLocations(cellAddr)!;
+      const cellName = appProxy.getCellName(hcl[0]!);
+      return {
+            ts,
+            name: cellName,
+            dna: cellAddr.dnaId.short,
+            arc: info.arc_size,
+            peers: info.current_number_of_peers,
+            total_peers: info.total_network_peers,
+            rounds: info.completed_rounds_since_last_time_queried,
+            bytes: info.bytes_since_last_time_queried,
+            fetch_bytes: info.fetch_pool_info.op_bytes_to_fetch,
+            fetch_ops: info.fetch_pool_info.num_ops_to_fetch,
+            agent: cellAddr.agentId.short,
+          }
+    })
+    console.table(logs);
+  }
+
+
+  /* */
+  dumpNetworkInfoLogs(cellIdStr?: string) {
+    console.log(`dumpNetworkInfoLogs() "${cellIdStr}"`);
+    const appProxy = this.hvms[0]![0];
+    let logMap = appProxy.networkInfoLogs;
+    if (cellIdStr) {
+      logMap = {};
+      logMap[cellIdStr] = appProxy.networkInfoLogs[cellIdStr]!;
+    }
+    for (const [cellIdStr, infoPairs] of Object.entries(logMap)) {
+      const cellAddr = CellAddress.from(cellIdStr);
+      const hcl = appProxy.getLocations(cellAddr)!;
+      const cellName = appProxy.getCellName(hcl[0]!);
+      console.log(`NetworfInfo logs of cell "${cellName}" | [${cellAddr.agentId.short}, ${cellAddr.dnaId.short}]`);
+      const logs = infoPairs
+        .map(([ts, info]) => {
+          return {
+            ts,
+            arc: info.arc_size,
+            peers: info.current_number_of_peers,
+            total_peers: info.total_network_peers,
+            rounds: info.completed_rounds_since_last_time_queried,
+            bytes: info.bytes_since_last_time_queried,
+            fetch_bytes: info.fetch_pool_info.op_bytes_to_fetch,
+            fetch_ops: info.fetch_pool_info.num_ops_to_fetch,
+          }
+        });
+      console.table(logs);
+    }
+  }
 
 
   // /** */
