642931130f3dd08f53e6370912204a9b948efc33
ddd-mtl <doute.d@gmail.com>
2025-10-21 14:00:40 -0400

    Fix network metrics API call in `HappMultiElement`, and bump versions to 0.32.13.

--- scripts/clean-npm.sh
+++ scripts/clean-npm.sh
@@ -12,6 +12,8 @@ rm packages/lit-happ/package-lock.json
 # example
 rm -rf example/webapp/node_modules/
 rm example/webapp/package-lock.json
-# example
+# example we-applet
 rm -rf example/we-applet/node_modules/
 rm example/we-applet/package-lock.json
+# example
+rm -rf example/node_modules/
