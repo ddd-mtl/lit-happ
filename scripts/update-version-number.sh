9db92e538ce0955e0a0d9d7e7a50f8e14de60a94
ddd-mtl <doute.d@gmail.com>
2025-11-19 11:37:08 -0500

    bump version numbers

--- scripts/update-version-number.sh
+++ scripts/update-version-number.sh
@@ -8,38 +8,20 @@ if [ $# != 1 ]; then
   exit 2
 fi
 
+update_package_version() {
+    local package_path="$1"
+    local new_version="$2"
 
-# Change cell-proxy/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./packages/cell-proxy/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./packages/cell-proxy/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./packages/cell-proxy/package.json
+    OLD_VER=$(awk -F ":" '/"version"/ {print $2}' "$package_path" | sed 's/"//g' | sed 's/,//g' | sed 's/ //g')
+    #echo "${package_path} ${OLD_VER} -> ${new_version}"
+    sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$new_version\"/" "$package_path"
+}
 
-# Change lit-happ/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./packages/lit-happ/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./packages/lit-happ/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./packages/lit-happ/package.json
 
-# Change we-utils/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./packages/we-utils/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./packages/we-utils/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./packages/we-utils/package.json
-
-# Change profiles-dvm/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./dvms/profiles/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./dvms/profiles/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./dvms/profiles/package.json
-
-# Change agent-directory/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./dvms/agent-directory/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./dvms/agent-directory/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./dvms/agent-directory/package.json
-
-# Change shared-ownership-dvm/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./dvms/shared-ownership/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./dvms/shared-ownership/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./dvms/shared-ownership/package.json
-
-# Change path-explorer/package.json
-OLD_VER=`awk -F ":" '/"version"/ {print $2}' ./dvms/path-explorer/package.json | sed 's/"//g' | sed 's/,//g' | sed 's/ //g'`
-echo "./dvms/path-explorer/package.json $OLD_VER -> $1"
-sed -i "s/\"version\": \"$OLD_VER\"/\"version\": \"$1\"/" ./dvms/path-explorer/package.json
+update_package_version "./packages/cell-proxy/package.json" $1
+update_package_version "./packages/lit-happ/package.json" $1
+update_package_version "./packages/we-utils/package.json" $1
+update_package_version "./dvms/profiles/package.json" $1
+update_package_version "./dvms/shared-ownership/package.json" $1
+update_package_version "./dvms/path-explorer/package.json" $1
+update_package_version "./dvms/agent-directory/package.json" $1
